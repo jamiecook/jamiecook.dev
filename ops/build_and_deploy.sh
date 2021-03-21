f7e5cd18c66a88667b6eafa6a14dd9ca957145ec
Jamie Cook <jamie.cook@veitchlister.com.au>
2021-03-21 16:46:30 +1000

    .

--- ops/build_and_deploy.sh
+++ ops/build_and_deploy.sh
@@ -5,14 +5,21 @@ set -eu
 OPS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 main() {
-    deploy_building_footprints
+    # deploy_building_footprints
+    deploy_school_catchments
+    firebase deploy
 }
 
 deploy_building_footprints() {
     cd ${OPS_DIR}/../gallery/building_activity
     yarn build
     cp -R build/* ../../public/v2/gallery/building_footprints/
-    firebase deploy
 }
 
+deploy_school_catchments() {
+    cd ${OPS_DIR}/../gallery/school_catchments
+    yarn build
+    mkdir ../../public/v2/gallery/school_catchments
+    cp -R build/* ../../public/v2/gallery/school_catchments/
+}
 main "$@"
